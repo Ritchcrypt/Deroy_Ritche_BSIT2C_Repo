c6838a826f7724f5d65b1053ce24ddd3438a7a27
Ritchcrypt <ritchd2003@gmail.com>
2025-03-25 22:30:04 +0800

    Your commit message

---
+++ Activities/Function - Activity 1/function.js
@@ -0,0 +1,17 @@
+function whatShallWear (temp) {
+    if (temp< 60) {
+        console.log("Wear a jacket")
+    }
+
+    else if (temp < 70) {
+        console.log("Wear a t-shirt");
+    }
+        else{
+            console.log("Wear a Sweater")
+        }
+    }
+
+
+    whatShallWear(80);
+    whatShallWear(70);
+    whatShallWear(60);
