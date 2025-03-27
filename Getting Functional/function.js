fd466d66f619c8446e0c71f8dc4dc23d4ab29e95
Ritchcrypt <ritchd2003@gmail.com>
2025-03-27 23:36:09 +0800

    Added file

---
+++ Getting Functional/function.js
@@ -0,0 +1,13 @@
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF");
+    } else {
+        console.log("name + says woof woof");
+    }
+}
+
+
+bark("ice", 10);
+bark("kopiko", 23);
+bark("panda", 28);
+bark("chester", 5);
